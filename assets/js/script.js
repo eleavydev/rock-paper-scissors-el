c66ce04d5251bd0be73d4fc5d6de5e219fb539da
eleavydev <elaineleavy@gmail.com>
2022-06-28 15:58:14 +0000

    Add code to add event listeners to all of the buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,33 @@
 
 /**
+ * Declare constants for DOM elements
+ * and possible choices (Rock, Paper, Scissors)
+ */
+ const buttons = document.getElementsByClassName("control");
+ const playerScore = document.getElementById("player-score")
+ const computerScore = document.getElementById("computer-score")
+ const choices = ["rock", "paper", "scissors"]
+ 
+ const clapSound = new Audio("assets/audio/clap.mp3");
+
+
+/**
+ * Once the DOM has finished loading, loop through all of the buttons and Add event listener. 
+ */
+ document.addEventListener("DOMContentLoaded", function() {
+    
+    for (let button of buttons) {
+       button.addEventListener("click", function() {
+               let playerChoice = this.getAttribute("data-choice");
+               alert(`You clicked Elaine test playerChoice is${playerChoice}`);
+               runGame(playerChoice);
+           });
+       }      
+
+   })
+
+
+/**
  * The main game function called when the user has selected a button. 
  * This function accepts one parameter, which is the data-choice value of selected button 
  * (which will be 0, 1 or 2 repesesenting Rock, Paper or Scissors)
