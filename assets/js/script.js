f9bd2ece03e3c3cd6a908a5230b2307ba5d319e8
eleavydev <elaineleavy@gmail.com>
2022-06-28 15:56:07 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+
+/**
+ * The main game function called when the user has selected a button. 
+ * This function accepts one parameter, which is the data-choice value of selected button 
+ * (which will be 0, 1 or 2 repesesenting Rock, Paper or Scissors)
+  */
+ function runGame(playerChoice) {
+    
+
+}
+
+
+/**
+ * The checkWinner function compares Player Choice and Machine choice 
+ * and uses a series of If Statements to run through logic 
+  */
+function checkWinner (playerChoice,machineChoice) {
+   
+
+}
+
+
+/**
+ * Calculates  a random Int between 0 and max which is parameter that is passed in on the fly
+ */
+function getRandomInt(max) {
+    return Math. floor(Math. random() * max);
+    }
+
+
+/**
+ * Gets the current Player score from the DOM and increments it by 1
+ */
+function incrementPlayerScore() {
+    
+
+}
+
+/**
+ * Gets the current Machine score from the DOM and increments it by 1
+ */
+function incrementMachineScore() {
+
+
+}
