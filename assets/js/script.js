2e357493717264f865744a65fbbf4c5cd6b33dd8
eleavydev <elaineleavy@gmail.com>
2022-07-06 17:08:10 +0000

    Tidy up assets folder

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,23 @@
 /**
  * Declare constants for DOM elements
- * and possible choices (Rock, Paper, Scissors)
+ * and possible choices (Rock, Paper, Scissors) 
  */
 const buttons = document.getElementsByClassName("control");
-const playerScore = document.getElementById("player-score");
-const machineScore = document.getElementById("machine-score");
 const playerImage = document.getElementById("player-image");
 const machineImage = document.getElementById("machine-image");
 const choices = ["rock", "paper", "scissors"];
+
 
 //Declare variables
 let text;
 let playerChoice;
 
-// const winnerModal = document.getElementsByClassName('winner-modal')[0];
-// const modalHeader = document.getElementsByClassName('modal-header')[0];
+/**
+ * Declare Modal constants to handle modal paragraph Text and Modal variables modal and span. 
+ */
 const modalParagraph = document.getElementsByClassName('modal-result')[0];
+var modal = document.getElementById("gameModal");
+var span = document.getElementsByClassName("close")[0];
 
 
 /**
@@ -31,23 +33,6 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
 })
-
-// /**
-//  * Close winner modal that is passed into the function
-//  */
-//  closeBtn.addEventListener('click', () => {
-//   closeModal(winnerModal);
-// });
-
-
-// Get the modal
-var modal = document.getElementById("gameModal");
-
-// // Get the button that opens the modal
-// var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
 
 
 /**
@@ -71,20 +56,13 @@ function playRound(playerChoice) {
 }
 
 /**
- * Calculates  a random Int between 0 and max which is parameter that is passed in on the fly
- */
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
-
-
-/**
  * The checkWinner function compares Player Choice and Machine choice 
  * and uses a series of If Statements to run through logic 
  */
 function checkWinner(playerChoice, machineChoice) {
 
-  // Run through logic to check which of the 7 Cases is true, alert user and call Increment Score function
+  // Run through logic to check which of the 7 Cases is true
+  // Set text and call Increment Score function passing in Round winner and text to be dispalyed in messages area. 
   switch (true) {
 
     // Case 1:Player and Machine have same choice (0 and 0, 1 and 1, 2 and 2)
@@ -134,9 +112,6 @@ function checkWinner(playerChoice, machineChoice) {
  */
 function incrementScore(roundWinner, text) {
 
-  // let oldPlayerScore = parseInt(document.getElementById("player-score").innerText);
-  // let oldMachineScore = parseInt(document.getElementById("machine-score").innerText);
-
   let oldPlayerScore = parseInt(document.getElementById("player-score").innerText);
   let oldMachineScore = parseInt(document.getElementById("machine-score").innerText);
 
@@ -160,34 +135,15 @@ function incrementScore(roundWinner, text) {
 
   //Test to see if either Score is 5 and call resetGame function
   if (document.getElementById("player-score").innerText == "5") {
-    // alert("Game over, Congrats You have won with Score of 5 - You rock!!");
-    // winnerModal.style.display = 'block';
-    // modalHeader.textContent = 'Congrats! You Won The Game!';
     modal.style.display = "block";
     modalParagraph.textContent = 'Congrats, You have won with Score of 5 - You rock!! Press X in Top Right corner to exit and play again. ';
     resetGame();
   } else if (document.getElementById("machine-score").innerText == "5") {
-    // alert("Game over - Machine has won with Score of 5, Better luck next time!");
-    // winnerModal.style.display = 'block';
-    // modalHeader.textContent = 'Hard Luck! You lost The Game!';
-    // modalParagraph.textContent = 'Hard Luck! You lost The Game!';
     modal.style.display = "block";
     modalParagraph.textContent = 'Machine has won with Score of 5, Better luck next time! Press X in Top Right corner to exit and play again. ';
     resetGame();
   }
 
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
 }
 
 
@@ -209,4 +165,25 @@ function resetGame() {
   //Reset Game Messages area 
   document.getElementById("game-messages").innerText = "";
 
+}
+
+
+/**
+ * Calculates a random Int between 0 and max which is parameter that is passed in on the fly
+ */
+ function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
